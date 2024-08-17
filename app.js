c1583f8bc9cb25fd435cde4368a83fc0b23aa17f
Raj Varsani <varasaniraj5@gmail.com>
2024-08-17 23:27:01 +0530

    Test commit
    Co-authored-by: Kshitij Ayush <@kshitij-404>

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ import express from "express";
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("This is Phalanx Demo Server");
+app.get("/", (_, res) => {
+  res.send("This is THE Phalanx Demo Server");
 });
 
 app.listen(port, () => {
