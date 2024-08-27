e97b9586e004fb973976413b23bee09d3b239080
Raj Varsani <varasaniraj5@gmail.com>
2024-08-27 23:31:33 +0530

    feat: add `phalanx` helper endpoints

--- routes/index.ts
+++ routes/index.ts
@@ -1,4 +1,5 @@
 import { Request, Response, Router } from "express";
+import { uuid } from "uuidv4";
 
 export const router = Router();
 
@@ -6,9 +7,51 @@ const demoController = (req: Request, res: Response) => {
   res.send(`${req.method} ${req.path} Works!`);
 };
 
+const tokens = {
+  admin: uuid(),
+  user1: uuid(),
+  user2: uuid(),
+};
+
+const userData = {
+  admin: {
+    firstName: "Admin",
+    lastName: "User",
+    email: "admin@phalanx.xyz",
+  },
+  user1: {
+    firstName: "User",
+    lastName: "One",
+    email: "user1@phalanx.xyz",
+  },
+  user2: {
+    firstName: "User",
+    lastName: "Two",
+    email: "user2@phalanx.xyz",
+  },
+};
+
+const validatePhalanxRequest = (req: Request, res: Response) => {
+  // *Validate Common Security Token
+  const commonSecurityToken = req.headers["phalanx-shared-secret"];
+  console.log("commonSecurityToken", commonSecurityToken);
+  console.log(
+    "process.env.PHALANX_SHARED_SECRET",
+    process.env.PHALANX_SHARED_SECRET
+  );
+  if (commonSecurityToken !== process.env.PHALANX_SHARED_SECRET) {
+    return res.status(401).send("Unauthorized Access");
+  }
+};
+
 router
-  .get("/test", demoController)
-  .post("/test", demoController)
+  .get("/phalanx/tokens", validatePhalanxRequest, (req, res) => {
+    res.send(tokens);
+  })
+  .post("/phalanx/user-data", validatePhalanxRequest, (req, res) => {
+    res.send(userData);
+  })
+
   .put("/test", demoController)
   .delete("/test", demoController)
   .get("/test/example", demoController)
